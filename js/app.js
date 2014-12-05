297a675e7471966907a9ee0d83f1c5de2d0e089f
garciadanny <dannygarcia.me@gmail.com>
2014-12-05 11:12:19 -0700

    create ember app

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Make the ember app available to the browser's javascript environment.
+window.Todos = Ember.Application.create();
+
+// This indicates that our app's ApplicationAdapter is an extension of DS.FixtureAdapter
+// Adapters are responsible for communicating with a source of data. i.e. APIs
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
