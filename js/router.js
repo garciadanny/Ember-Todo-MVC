50b5a5f8e1aa6bd8779487ade5ee22bdd5100f8f
garciadanny <dannygarcia.me@gmail.com>
2014-12-05 15:59:54 -0700

    Create nested route/template list of todo items

--- js/router.js
+++ js/router.js
@@ -1,10 +1,20 @@
 Todos.Router.map(function() {
   // when the url path matches '/', use the 'todos' template.
-  this.resource('todos', {path: '/'});
+  this.resource('todos', {path: '/'}, function() {
+    // additional child routes will go here
+  });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
+  }
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    // notice this is the plural version of todo.
+    // Indicating this uses the same model as the TodosRoute.
+    return this.modelFor('todos');
   }
 });
