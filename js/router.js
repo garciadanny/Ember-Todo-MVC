1eaf752414903245c76c4e71ef363fdace7d90a2
garciadanny <dannygarcia.me@gmail.com>
2014-12-09 16:51:34 -0700

    update router & add route to display active todos by resuing the todos/index template

--- js/router.js
+++ js/router.js
@@ -2,6 +2,7 @@ Todos.Router.map(function() {
   // when the url path matches '/', use the 'todos' template.
   this.resource('todos', {path: '/'}, function() {
     // additional child routes will go here
+    this.route('active');
   });
 });
 
@@ -16,5 +17,16 @@ Todos.TodosIndexRoute = Ember.Route.extend({
     // notice this is the plural version of todo.
     // Indicating this uses the same model as the TodosRoute.
     return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
   }
 });
