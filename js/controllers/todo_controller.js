ce29aad65e9bc5d86bbfd8622701009361259cd4
garciadanny <dannygarcia.me@gmail.com>
2014-12-16 14:58:17 -0700

    add comments to describe behavior

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -14,6 +14,7 @@ Todos.TodoController = Ember.ObjectController.extend({
       }
     },
     removeTodo: function() {
+      // Get the model data, the record.
       var todo = this.get('model');
       todo.deleteRecord();
       todo.save();
@@ -28,7 +29,10 @@ Todos.TodoController = Ember.ObjectController.extend({
 
     if (value === undefined) {
       // if the checkbox hasn't been clicked
-      // i.e. if there is no value
+      // i.e. if there has no `value`
+      // We need this when the DOM is constructed. At that point, a user hasn't clicked on the checkmark
+      // but ember calls isCompleted to check if the item has already been completed and saved
+      // to the data source at another point in time.
       return model.get('isCompleted');
     }
     else {
